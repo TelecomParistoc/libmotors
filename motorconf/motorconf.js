7fe65934534ed65be53647d0e676d077feec267b
superpingu <arnaudbonetti@me.com>
2018-01-28 18:41:59 +0100

    fixed negative number arguments and help

--- motorconf/motorconf.js
+++ motorconf/motorconf.js
@@ -48,6 +48,9 @@
     }
     if (operation === 'write') {
       if (isNaN(val)) {
+        val = process.argv.pop();
+      }
+      if (isNaN(val)) {
         console.log('A valid value must be specified.');
         process.exit(1);
       }
@@ -60,16 +63,16 @@
     console.log('   <operation> : use "write" to write a new parameter value in flash,');
     console.log('                  and "read" to get the current parameter value');
     console.log('   <parameter> : a parameter name among : ');
-    console.log('       wheelsgap         : distance between the encoder wheels in mm');
-    console.log('       tickspermeter     : number of ticks when the robot moves 1m forward');
-    console.log('       linacc, angacc    : accelerations during speed up and slow down (mm/s^2 and deg/s^2)');
-    console.log('       linspeed, angspeed: cruise speeds (mm/s and deg/s)');
-    console.log('       linp, lini, lind  : linear PID coefficients');
-    console.log('       angp, angi, angd  : angular PID coefficients');
-    console.log('       ldir, lwheeldir   : left side hardware switches (0 or 1)');
-    console.log('       rdir, rwheeldir   : right side hardware switches (0 or 1)');
-    console.log('       distance          : distance to move (write) or traveled (read) in mm, signed');
-    console.log('       heading           : heading to turn (write) or current heading (read), 0 to 360 CCW');
+    console.log('      wheelsgap         : distance between the encoder wheels in mm');
+    console.log('      tickspermeter     : number of ticks when the robot moves 1m forward');
+    console.log('      linacc, angacc    : accelerations during speed up and slow down (mm/s^2 and deg/s^2)');
+    console.log('      linspeed, angspeed: cruise speeds (mm/s and deg/s)');
+    console.log('      linp, lini, lind  : linear PID coefficients');
+    console.log('      angp, angi, angd  : angular PID coefficients');
+    console.log('      ldir, lwheeldir   : left side hardware switches (0 or 1)');
+    console.log('      rdir, rwheeldir   : right side hardware switches (0 or 1)');
+    console.log('      distance          : distance to move (write) or traveled (read) in mm, signed');
+    console.log('      heading           : heading to turn to (write) or current heading (read), absolute 0 to 360 CCW');
     console.log('   [value]     : required for write, ignored for read : new parameter value');
     return console.log('');
   }).parse(process.argv);
